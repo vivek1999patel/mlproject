422e317837ecd39f6db69181e01b8c69d1f14805
Vivek Patel <43326066+vivek1999patel@users.noreply.github.com>
2025-10-15 20:05:06 -0400

    Added Model Trainer Component

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -41,7 +41,7 @@ class DataTransformation:
                 ]
             )
 
-            cat_pipeline=Pipeline(
+            cat_pipeline= Pipeline(
                 steps=[
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
